a2a40f4c0e9c3c188fcf5b53fff04c2275e02fae
Allen Garvey <allengarvey@hotmail.com>
2016-05-03 21:43:51 -0400

    add test for apple numbers exported csv file

--- spec/file_spec.js
+++ spec/file_spec.js
@@ -1,16 +1,21 @@
 var fs = require('fs');
 var CSV = require(__dirname + '/../dist/csv-parser-node.js');
-
+var mock_dir = __dirname + '/mock/';
 var parse = CSV.parse;
-
-var testFile1 = fs.readFileSync(__dirname + '/mock/test.csv', 'utf8');
 
 
 describe("CSV File Test Suite", function() {
-  	it("Test CSV file", function() {
+  	it("Test Excel CSV file", function() {
+  		var testFile1 = fs.readFileSync(mock_dir + 'excel_test.csv', 'utf8');
   		expect(parse(testFile1)).toEqual([ [ 'Hello, there', 'Cell 2' ], [ '"Hello, there with quotes"', 'Some more information' ], [ '[bracketsn and such]', 'Commas, anyone? \'I didn\'t think so, baby,"She said"\',' ] ]);
   	});
+
+  	it("Test Excel CSV file", function() {
+  		var testFile1 = fs.readFileSync(mock_dir + 'numbers_test.csv', 'utf8');
+  		expect(parse(testFile1)).toEqual([ [ 'Header', 'Header2', '' ], [ 'Some text', 'One, two, “three”', 'hello, there' ], [ 'Second column', ',there “there”', '' ], ['', 'empty before', ''] ]);
+  	});
 });
+
 
 
 
